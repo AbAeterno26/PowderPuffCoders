874d137ae2cec0ee152cc631dd1d1fde398ef0c0
AbAeterno26 <nathanchress@hotmail.com>
2023-06-08 14:56:35 +0200

    update grid.py and main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from sys import argv
 if __name__ == "__main__":
 
     if len(argv) == 1:
-        print("Usage: python main.py [aminoacid]")
+        print("Usage: python main.py [protein]")
         exit(1)
     
-    # loading the amino acid
-    aminoacid = argv[1]
-    with open(aminoacid) as f:
-        for line in f:
-            print(f"The loaded amino acid is: {line}")
+    # Loading the protein
+    protein_file = argv[1]
+    with open(protein_file) as f:
+        for protein in f:
+            print(f"The loaded protein is: {protein}")
     
         # Loading and printing the grid
-        grid_obj = grid.Grid(len(line))
+        grid_obj = grid.Grid(len(protein))
         grid_obj.print_grid()
