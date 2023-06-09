6961628f47e3c20fc0c891368e7b053e38a10d7e
suus-stack <suzefrikk003@gmail.com>
2023-06-09 11:38:12 +0200

    Updated the main.py

--- code/classes/tryout.py
+++ code/classes/tryout.py
@@ -1,14 +1,24 @@
 import tkinter as tk
+import random 
 
 grid = tk.Tk()
 
+width = 3
+height = 3
+
+# loop over each amino-acid, make a label for each amino-acid and then generate the row and column 
+# index to position it in the grid and display. 
+
 # Label class is for text or images(widget)
 # Controlling the width and height of a label with the width and height parameters
-mylabel1 = tk.Label(grid, text='P', bg="blue", width=1, height=1)
-mylabel2 = tk.Label(grid, text='H', bg="red", width=1, height=1)
+
+mylabel1 = tk.Label(grid, text='P', bg="blue", width=width, height=height)
+mylabel2 = tk.Label(grid, text='H', bg="red", width=width, height=height)
 
 # also possible to add the text to the window by using 
 # mylabel1.pack()
+
+# Generate random row and column in de range van de lengte van het eiwit (0 tm lengte eiwit)
 
 mylabel1.grid(row=0, column=1)
 mylabel2.grid(row=1, column=1)
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from code.classes import grid
+from code.classes import p
+from code.classes import h
+from code.algorithms import randomise 
 from sys import argv
+import tkinter as tk 
 
 
 if __name__ == "__main__":
-
+    # Check for the correct command line input
     if len(argv) == 1:
         print("Usage: python main.py [protein]")
         exit(1)
@@ -13,7 +17,22 @@ if __name__ == "__main__":
     with open(protein_file) as f:
         for protein in f:
             print(f"The loaded protein is: {protein}")
-    
-        # Loading and printing the grid
-        grid_obj = grid.Grid(len(protein))
-        grid_obj.print_grid()
+            # Create grid object
+            grid_obj = grid.Grid(len(protein))
+
+            for aminoacid in protein:
+                #Create class object from aminoacid
+                if aminoacid == 'P':
+                    amino = p.Polair()
+                elif aminoacid == 'H':
+                    amino = h.Hydrofoob()
+                else:
+                    #This is reserved for the C class
+                    pass
+
+                # Call random algorithm to solve protein fold
+                # Create widget for amino acid visualisation
+                amino_acid_label = tk.Label(grid, text=amino.text, bg=amino.color, width=1, height=1)
+                row, column = randomise.add_bond(amino, len(protein))
+
+        # Print the grid of the entire protein
