dad96ffc0ecb81d8107c18e2d8600cb853557a09
isahio <m.mulder1200@gmail.com>
2023-06-09 11:34:16 +0200

    grid update

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -1,17 +1,21 @@
 class Grid():
     # width equals the length of the protein string 
-    def __init__(self, width):
-        self.width = width
+    def __init__(self, aminoacids: list):
+        self.bonds = []
 
-    def compute_score(self):
+    def compute_score(self) -> float:
+        """ Computes the total stability score for the entire protein """
+        # Loop over self.bonds and sum all the scores
         pass
 
     def add_bond(self, amino):
-        pass
+        """ Checks what the optimal location is for the amino acid to be placed and returns a row and column index """
+        self.bonds.append(amino)
 
     def check_grid(self):
-        self.board = [['_' for i in range(self.width)] for j in range(self.width)]
+        # self.board = [['_' for i in range(self.width)] for j in range(self.width)]
 
     def print_grid(self):
-        self.check_grid()
-        print(self.board)
+        """ Prints the grid for one protein """
+        # self.check_grid()
+        # print(self.board)
