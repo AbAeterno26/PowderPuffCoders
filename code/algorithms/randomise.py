d14f86cc80baca92a23ac93161de50f184d28a42
suus-stack <suzefrikk003@gmail.com>
2023-06-09 11:49:06 +0200

    update randomise and main.py

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -1,12 +1,11 @@
 import random
 
-def add_bond(self, amino, protein: int):
+def gen_location(self, amino, protein: int):
     """ Generates a random location for the amino acid to be placed """
-    # self.bonds.append(amino)
 
     # Generate random location for the amino acid to be placed
     row = random.randint([0, protein])
-    col = random.randint([0, protein])
+    column = random.randint([0, protein])
 
-    return row, col
+    return row, column
 
--- code/classes/p.py
+++ code/classes/p.py
@@ -1,4 +1,8 @@
 class Polair():
-    def __init__(self):
+    def __init__(self, row, column):
         self.color = "blue"
         self.text = "P"
+        self.row = row
+        self.column = column
+
+        
