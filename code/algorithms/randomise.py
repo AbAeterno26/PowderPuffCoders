6f7d6fa75ef29ef6a4910de94adcf506f80d75db
isahio <m.mulder1200@gmail.com>
2023-06-12 12:18:14 +0200

    randomise update algorithm

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -1,11 +1,9 @@
 import random
 
-class randomise:
-    @staticmethod
-    def gen_location(protein_length):
-        grid_size = protein_length
+def gen_location(protein_length):
+    grid_size = protein_length
 
-        # Generate a random row and column
-        row = random.randint(0, grid_size - 1)
-        column = random.randint(0, grid_size - 1)
-        return row, column
+    # Generate a random row and column
+    row = random.randint(0, grid_size - 1)
+    column = random.randint(0, grid_size - 1)
+    return row, column
