555933fa1efef69ce943772a9197f6f5bbcb2b63
isahio <m.mulder1200@gmail.com>
2023-06-12 10:59:55 +0200

    updated randomise.py

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -8,11 +8,25 @@ def gen_location(self, amino, protein: int, protein_length, occupied_pos, last_p
     random.shuffle(amino_move)
 
     for move in amino_move:
-        new_pos = (last_pos[0] + move[0], last_pos[1] + move[1])        
+        new_pos = (last_pos[0] + move[0], last_pos[1] + move[1])    
 
-    # Generate random location for the amino acid to be placed
-    row = random.randint([0, protein])
-    column = random.randint([0, protein])
+        if (0 <= new_pos[0] < protein_length and 0 <= new_pos[1] < protein_length
+            and new_pos not in occupied_pos):
+            return new_pos
 
-    return row, column
+def test_gen_location():
+    """Test the gen_location function."""
+    
+    protein = 5
+    protein_len = 5
+    occ_pos = [(0, 0), (0, 1), (1, 1)]
+    last_position = (1, 1)
+
+    # Testing gen_location function
+    new_position = gen_location(None, protein, protein_len, occ_pos, last_position)
+
+    print(f"New position generated: {new_position}")
+
+# Now call the test function
+test_gen_location()
 
