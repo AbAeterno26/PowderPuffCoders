2d655349fa7a4b6206e37523bd884340c2f5f597
isahio <m.mulder1200@gmail.com>
2023-06-15 15:24:00 +0200

    update main

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -1,6 +1,4 @@
 import random
-
-
 def fold_protein(grid):
     """ Folds an entire protein by generating random directions """
     # Right, left, down, up
@@ -10,22 +8,18 @@ def fold_protein(grid):
     
     # Keep connecting amino acids until the whole protein is folded
     for amino in grid.amino_acids:
-        # Check that the location of the amino acid is not already in use
-        while True:
-            direction = random.choice(directions)
-            next_pos = last_pos[0] + direction[0], last_pos[1] + direction[1]
+        random.shuffle(directions) # shuffle the directions 
+        for direction in directions:
+            next_pos = (last_pos[0] + direction[0], last_pos[1] + direction[1])
 
             # Check if the location falls outside the grid size (AKA length of the protein)
             if grid.is_valid(next_pos, used_pos):
-<<<<<<< HEAD
-=======
-                print("YAY ESCAPING THE INFINITE WHILE LOOOOOOOOOOOOOOOOOO....P!")
->>>>>>> 8722d4301459d0d55247fff105e92ba9f7d11e70
-                break
-        
-        # Update the location of the amino acid
-        amino.update_loc(next_pos)
+                # Update the location of the amino acid
+                amino.update_loc(next_pos)
 
-        # Update the last position for the next amino acid and add to in use locations
-        last_pos = next_pos
-        used_pos.add(last_pos)
+                # Update the last position for the next amino acid and add to in use locations
+                last_pos = next_pos
+                used_pos.add(last_pos)
+                break
+        else:
+            raise ValueError("No valid moves for current amino acid.")
--- main.py
+++ main.py
@@ -27,8 +27,8 @@ if __name__ == "__main__":
     visualize.plot_grid(grid_obj.amino_acids)
 
     # Compute the score for the folding of the protein
-    # grid_obj.compute_score()
+    grid_obj.compute_score()
 
-    # grid_obj.output_to_csv(protein_file)
+    grid_obj.output_to_csv(protein_file)
 
     grid_obj.display_rules()
