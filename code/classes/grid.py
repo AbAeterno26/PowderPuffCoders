780f6af28aaa32287f11086209243b39e9e2cdaa
suus-stack <suzefrikk003@gmail.com>
2023-06-16 11:58:00 +0200

    bla

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -19,7 +19,7 @@ class Grid():
         self.score = 0
 
         for location, amino_acid in self.amino_locations.items():
-<<<<<<< HEAD
+
             # Reset the number of neighbours to 0 for each amino acid
             neighbours = 0
             for x_way, y_way in self.directions:
@@ -33,20 +33,7 @@ class Grid():
             if neighbours == 4:
                 self.score += self.calculate_bond_score(amino_acid, next_amino)
                 print(f"THE SCORE IS: {self.score}")
-=======
-            print(f"Checking amino acid at {location} with type {amino_acid.text}")
-            for dx, dy in directions_to_consider:
-                neighbour_location = (location[0] + dx, location[1] + dy)
-                neighbour_amino = self.amino_locations.get(neighbour_location)
-                if neighbour_amino and neighbour_amino.text != "P" and amino_acid.text != "P":
-                    bond_score = self.calculate_bond_score(amino_acid, neighbour_amino)
-                    print(f"Bond score with neighbour at {neighbour_location} is {bond_score}")
-                    self.score += bond_score
 
-        print(f"THE SCORE IS: {self.score}")
-        return self.score
-
->>>>>>> 8d8d1013e25d75f0da66bc352362d09be7c29083
 
     def calculate_bond_score(self, amino1, amino2) -> int:
         """
@@ -63,8 +50,7 @@ class Grid():
         else:
             return 0
     
-<<<<<<< HEAD
-=======
+
     def check_neighbours():
         pass
         
@@ -83,33 +69,8 @@ class Grid():
                 
                 self.max_grid_size = len(protein)
                 # Create interface object to visualize in Tkinter
-<<<<<<< HEAD
-                interface = tk.Tk()
-                amino_label = set()
-                for aminoacid in protein:
-                    # Create class object from amino acid, which is the key in the dict
-                    if aminoacid == 'P':
-                        amino = amino_cat.Amino("P", "blue")
-                        print("YY")
-                        amino_label = 'P'
-                    elif aminoacid == 'H':
-                        amino = amino_cat.Amino("H", "red")
-                        amino_label = 'H'
-                    else:
-                        amino = amino_cat.Amino("C", "green")
-                        amino_label = 'C'
-
-                    # Create widget for amino acid visualisation as value in dict
-                    amino_acid_label = tk.Label(interface, text=amino.text, bg=amino.color, width=3, height=3)
-                self.amino_acids[amino] = amino_label
-                print(self.amino_acids)
-                self.max_grid_size = len(protein)
-
-                    # Fill dictionary with amino acid location as key and the amino acid itself as value
-                self.amino_locations[amino._location] = amino
-        
-=======
                 # interface = tk.Tk()
+
                 for i, aminoacid in enumerate(protein):
                     # Create class object from amino acid, which is the key in the dict
                     if aminoacid == 'P':
