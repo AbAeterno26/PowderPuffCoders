203207aad4df291af4bb9a6ac7af53795bf9c299
suus-stack <suzefrikk003@gmail.com>
2023-06-19 15:00:24 +0200

    randomise now turned into class so SA can inherit

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -1,40 +1,45 @@
 import random
 
+class Random():
+    def __init__(self, grid):
+        self.grid = grid
+        
 
-def fold_protein(grid):
-    """ Folds an entire protein by generating random directions """
-    # Right, left, down, up
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    used_pos = set()
-    
-    # Keep connecting amino acids until the whole protein is folded
-    for amino_id, amino in grid.amino_acids.items():
-        location = amino._location
+    def fold_protein(self):
+        """ Folds an entire protein by generating random directions """
+        
+        # Right, left, down, up
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        used_pos = set()
+        
+        # Keep connecting amino acids until the whole protein is folded
+        for amino_id, amino in self.grid.amino_acids.items():
+            location = amino._location
 
-        # Check that the location of the amino acid is not already in use
-        while True:
-            direction = random.choice(directions)
-            next_pos = location[0] + direction[0], location[1] + direction[1]
-            
-            # Check if the location falls outside the grid size (AKA length of the protein)
-            if grid.is_valid(next_pos, used_pos):
-                # Update the location of the amino acid
-                amino.update_loc(next_pos)
+            # Check that the location of the amino acid is not already in use
+            while True:
+                direction = random.choice(directions)
+                next_pos = location[0] + direction[0], location[1] + direction[1]
+                
+                # Check if the location falls outside the grid size (AKA length of the protein)
+                if self.grid.is_valid(next_pos, used_pos):
+                    # Update the location of the amino acid
+                    amino.update_loc(next_pos)
 
-                # Add the updated location to the list of locations of all amino-acids
-                grid.locations.append(amino._location)
+                    # Add the updated location to the list of locations of all amino-acids
+                    self.grid.locations.append(amino._location)
 
-                # Add position to used locations
-                used_pos.add(next_pos)
-                grid.locations.append(amino._location)
-                break
-            
-            location = next_pos
+                    # Add position to used locations
+                    used_pos.add(next_pos)
+                    self.grid.locations.append(amino._location)
+                    break
+                
+                location = next_pos
 
-        # Check what the move was and add it to the history of moves
-        if direction[0] != 0:
-            grid.history.append(direction[0])
-        elif direction[1] == 1:
-            grid.history.append("2")
-        elif direction[1] == -1:
-            grid.history.append("-2")
+            # Check what the move was and add it to the history of moves
+            if direction[0] != 0:
+                self.grid.history.append(direction[0])
+            elif direction[1] == 1:
+                self.grid.history.append("2")
+            elif direction[1] == -1:
+                self.grid.history.append("-2")
