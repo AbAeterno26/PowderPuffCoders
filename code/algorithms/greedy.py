60be239f93d9920c2f8d6a41a6283c5cdc190197
isahio <m.mulder1200@gmail.com>
2023-06-26 17:32:57 +0200

    greedy update

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -2,15 +2,15 @@ import random
 
 class Greedy:
     def __init__(self, grid):
-        self.grid = grid
+        self.best_grid = grid
         self.directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
     def execute(self):
         location = (0, 0)
-        self.grid.amino_acids[0].update_loc(location)
-        self.grid.locations.add(location)
+        self.best_grid.amino_acids[0].update_loc(location)
+        self.best_grid.locations.add(location)
 
-        for amino_id, amino in self.grid.amino_acids.items():
+        for amino_id, amino in self.best_grid.amino_acids.items():
             # Initialize variables
             min_score = float('inf')
             best_direction = None
@@ -22,11 +22,11 @@ class Greedy:
             for direction in random_directions:
                 next_pos = (location[0] + direction[0], location[1] + direction[1])
                 
-                if self.grid.is_valid(next_pos):
+                if self.best_grid.is_valid(next_pos):
                     # Temporarily place the amino acid in the grid and calculate the score
                     amino.update_loc(next_pos)
-                    self.grid.locations.add(next_pos)
-                    score = self.grid.compute_score()
+                    self.best_grid.locations.add(next_pos)
+                    score = self.best_grid.compute_score()
 
                     if score < min_score:
                         min_score = score
@@ -34,7 +34,7 @@ class Greedy:
 
                     # Remove the amino acid from the temporary location
                     amino.update_loc(None)
-                    self.grid.locations.remove(next_pos)
+                    self.best_grid.locations.remove(next_pos)
 
             # If no direction was found that improves the score, take a random direction
             if best_direction is None:
@@ -42,12 +42,12 @@ class Greedy:
 
             next_pos = (location[0] + best_direction[0], location[1] + best_direction[1])
             amino.update_loc(next_pos)
-            self.grid.locations.add(next_pos)
+            self.best_grid.locations.add(next_pos)
             location = next_pos
 
-            if amino_id != self.grid.max_grid_size - 1:
-                self.grid.add_move(best_direction, amino_id)
+            if amino_id != self.best_grid.max_grid_size - 1:
+                self.best_grid.add_move(best_direction, amino_id)
 
                 
     def get_best_configuration(self):
-        return self.grid
+        return self.best_grid
