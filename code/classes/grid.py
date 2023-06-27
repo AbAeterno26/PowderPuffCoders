b5916a849d5afbae0a066568fe3d716f4052eda7
AbAeterno26 <nathanchress@hotmail.com>
2023-06-27 14:51:43 +0200

    merge

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -85,11 +85,7 @@ class Grid():
             return -5
         else:
             return 0
-<<<<<<< HEAD
         
-=======
-
->>>>>>> ac737cfb917fc266f983eea6e0b1333497610a3f
     def is_hydrogen_bond(self, amino1, amino2) -> bool:
         """ Checks if there is a hydrogen bond between two given amino acids """
         if self.calculate_bond_score(amino1, amino2) < 0:
